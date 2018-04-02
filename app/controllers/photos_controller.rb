4a0ac0068dbe78942a50626e8e238fff76913563
juwanpetty <jchpetty@gmail.com>
2018-04-02 12:16:16 -0400

    Create views for Photos Controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,49 @@
 class PhotosController < ApplicationController
+    before_action :find_photo, only: [:show, :edit, :update, :destroy]
+
+    def index
+    end
+
+    def new 
+        @photo = Photo.new
+    end
+
+    def create
+        @photo = Photo.new photo_params
+
+        if @photo.save
+            redirect_to @photo, notice: "Photo saved successfully"
+        else 
+            render 'new', notice: "Photo was not saved successfully"
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        if @photo.save
+            redirect_to @photo, notice: "Photo saved successfully"
+        else 
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @photo.destroy
+        redirect_to photo_path
+    end
+
+    private
+
+    def photo_params
+        params.require(:photo).permit(:location, :description)
+    end
+
+    def find_photo 
+        @photo = Photo.find(params[:id])
+    end
 end
